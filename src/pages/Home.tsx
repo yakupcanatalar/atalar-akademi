e9024f57067a989f0949bbb38a5450ad449d5c32
yakupcanatalar <atalaryakupcan@gmail.com>
2025-05-12 17:56:20 +0300

    Added Example Intro Video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,26 @@ const Home: React.FC = () => {
       <section className="hero card">
         <h1 className="hero-title">Atalar LGS Akademi ile Matematiği Keşfedin</h1>
         <p className="hero-description">
-          Matematik, sadece formüllerden ibaret değildir. Deneyimli öğretmen Sena Atalar ile öğrenciler; analitik düşünmeyi, problem çözmenin keyfini ve matematiğin hayatın içindeki yerini keşfeder. Matematiğe olan ilgilerini artırıyor, özgüvenlerini pekiştiriyor ve onları başarıya taşıyorum.</p>
+          Matematik, sadece formüllerden ibaret değildir. Deneyimli öğretmen Sena Atalar ile öğrenciler; analitik düşünmeyi, problem çözmenin keyfini ve matematiğin hayatın içindeki yerini keşfeder. Matematiğe olan ilgilerini artırıyor, özgüvenlerini pekiştiriyor ve onları başarıya taşıyorum.
+        </p>
       </section>
 
-      {/* Student Achievements */}
       <section className="achievements card">
         <h2 className="section-title">Sunulan İçerikler / Hizmetler</h2>
         <Highlights />
+      </section>
+
+            {/* YouTube Video Section */}
+      <section className="video-section card">
+        <h2 className="section-title">Tanıtım Videosu</h2>
+        <div className="video-container small-video">
+          <iframe
+            src="https://www.youtube.com/embed/qcJtPCje3sY"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </section>
 
       {/* Live Class Schedule */}
